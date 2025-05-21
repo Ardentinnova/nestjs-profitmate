5799fc39f0dd29a09f642930c99afb363203c762
shArdent <eartkuad5@gmail.com>
2025-05-21 14:46:29 +0700

    add description to transaction

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -4,43 +4,48 @@ import { ExpenseCategory, TransactionType } from 'generated/prisma';
 import { IsBigIntString } from 'src/common/validators/is-bigint-string.validator';
 
 export class CreateTransactionDto {
-  @ApiProperty({
-    description: 'Id Periode',
-    example: 'a9c0c81a-d145-4b92-aeda-8a774b15333a',
-  })
-  @IsString()
-  @IsNotEmpty()
-  periodId: string;
+    @ApiProperty({
+        description: 'Id Periode',
+        example: 'a9c0c81a-d145-4b92-aeda-8a774b15333a',
+    })
+    @IsString()
+    @IsNotEmpty()
+    periodId: string;
 
-  @ApiProperty({
-    description: 'Nama transaksi',
-    example: 'Pembelian bahan baku terigu',
-  })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
+    @ApiProperty({
+        description: 'Nama transaksi',
+        example: 'Pembelian bahan baku terigu',
+    })
+    @IsString()
+    @IsNotEmpty()
+    name: string;
 
-  @ApiProperty({
-    description: 'Nominal transaksi (dalam string)',
-    example: '1600000',
-  })
-  @IsBigIntString({ message: 'Amount harus berupa string angka valid' })
-  @IsNotEmpty()
-  amount: string;
+    @ApiProperty({ example: "Pembelian yang digunakan untuk membeli terigu sebanyak 5kg (disuruh mang udin)" })
+    @IsString()
+    @IsOptional()
+    description: string
 
-  @ApiProperty({
-    description: 'Jenis transaksi (INCOME/EXPENSE)',
-    example: TransactionType.EXPENSE,
-  })
-  @IsEnum(TransactionType)
-  @IsNotEmpty()
-  type: TransactionType;
+    @ApiProperty({
+        description: 'Nominal transaksi (dalam string)',
+        example: '1600000',
+    })
+    @IsBigIntString({ message: 'Amount harus berupa string angka valid' })
+    @IsNotEmpty()
+    amount: string;
 
-  @ApiPropertyOptional({
-    description: 'Jenis pengeluaran  (OPERATIONAL/OTHER)',
-    example: ExpenseCategory.OPERATIONAL,
-  })
-  @IsEnum(ExpenseCategory)
-  @IsOptional()
-  expenseCategories?: ExpenseCategory;
+    @ApiProperty({
+        description: 'Jenis transaksi (INCOME/EXPENSE)',
+        example: TransactionType.EXPENSE,
+    })
+    @IsEnum(TransactionType)
+    @IsNotEmpty()
+    type: TransactionType;
+
+    @ApiPropertyOptional({
+        description: 'Jenis pengeluaran  (OPERATIONAL/OTHER)',
+        example: ExpenseCategory.OPERATIONAL,
+    })
+    @IsEnum(ExpenseCategory)
+    @IsOptional()
+    expenseCategories?: ExpenseCategory;
 }
